becf4b6d524ac62fc9515f927b3acc4a091fa894
David Grayson <dkgrayson@gmail.com>
2023-12-21 13:50:17 -0500

    release: 2.5

--- src/classes/Game.js
+++ src/classes/Game.js
@@ -8,43 +8,10 @@ import { PlayerPhysics } from './PlayerPhysics';
 import { Light } from './Light';
 import { Timer } from './Timer';
 import { getTime } from './Helpers';
-import level1 from '../../levels/1.json';
-import level2 from '../../levels/2.json';
-import level3 from '../../levels/3.json';
-import level4 from '../../levels/4.json';
-import level5 from '../../levels/5.json';
-import level6 from '../../levels/6.json';
-import level7 from '../../levels/7.json';
-import level8 from '../../levels/8.json';
-import level9 from '../../levels/9.json';
-import level10 from '../../levels/10.json';
-import level11 from '../../levels/11.json';
-import level12 from '../../levels/12.json';
-import level13 from '../../levels/13.json';
-import level14 from '../../levels/14.json';
-import level15 from '../../levels/15.json';
-import level16 from '../../levels/16.json';
 
 export class Game {
-  constructor() {
-    this.levels = [
-      level1,
-      level2,
-      level3,
-      level4,
-      level5,
-      level6,
-      level7,
-      level8,
-      level9,
-      level10,
-      level11,
-      level12,
-      level13,
-      level14,
-      level15,
-      level16
-    ];
+  constructor(levels) {
+    this.levels = levels;
     this.currentLevelIndex = 0;
     this.gameState = 'starting';
     this.loadingLevel = false;
